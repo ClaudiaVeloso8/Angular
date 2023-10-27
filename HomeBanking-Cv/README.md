49e0817f3255f6d09dad1a6131774208f188a902
ClaudiaVeloso8 <132751861+ClaudiaVeloso8@users.noreply.github.com>
2023-10-27 07:21:07 +0100

    first commit

---
+++ HomeBanking-Cv/README.md
@@ -0,0 +1,16 @@
+# Home Banking
+Pagina de login com lista de movimentos, deposito de fundos e retirada de fundos.
+
+## :camera_flash: Screenshoots
+<img src="/img1.png" width="260">&emsp;
+
+## Technologies
+* Angular
+* Javascript
+* Typescript
+* HTML
+* CSS
+
+## Contacto
+
+* Raphael de Mello Garrido da Silva [linkedin](https://www.linkedin.com/in/raphaelmgs/)
---
+++ HomeBanking-Cv/src/app/home/home.component.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+});
